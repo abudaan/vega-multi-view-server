64ba399da01fdc188f85e6f3fbd551e46b514033
abudaan <daniel@abumarkub.net>
2017-09-10 22:00:19 +0200

    initial import

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Response;
 
 $useTwig = getenv('TWIG') == 1;
-error_log("[twig] $useTwig \n", 3, __DIR__.'/php.log');
+// error_log("[twig] $useTwig \n", 3, __DIR__.'/php.log');
 
 $loader = new Twig_Loader_Filesystem( __DIR__.'/twig');
 $twig = new Twig_Environment($loader, array(
@@ -17,24 +17,6 @@ $yaml = new Parser();
 $config = $yaml->parse(file_get_contents(__DIR__.'/config.yml'));
 
 $app = new Silex\Application();
-
-
-// statically serve javascript and css
-$app->get('/assets/{file}', function ($file) use ($app) {
-    error_log("[assets] $file \n", 3, __DIR__.'/php.log');
-    $file = __DIR__.'/assets/'.$file;
-    $stream = function () use ($file) {
-        readfile($file);
-    };
-    $contentType;
-    if (strpos($file, '.css') != False) {
-        $contentType = 'text/css';
-    } else if (strpos($file, '.js') != False) {
-        $contentType = 'application/javascript';
-    }
-    return $app->stream($stream, 200, array('Content-Type' => $contentType));
-});
-
 
 $app->get('/{views}', function ($views) use ($app, $twig, $yaml, $config) {
     $ids = explode('/', $views);
@@ -69,5 +51,4 @@ $app->get('/{views}', function ($views) use ($app, $twig, $yaml, $config) {
 })
 ->assert('views', '.*');
 
-// return $app;
 $app->run();
